bc49e4a8fa63f1181bbbbce3450bfc2d9143d7aa
niteshsingh0233 <niteshsingh0233@gmail.com>
2022-09-19 23:36:22 +0530

    starting with the project

---
+++ portfolio/src/index.js
@@ -0,0 +1,7 @@
+// Not necessary to import React in newer versions.
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
